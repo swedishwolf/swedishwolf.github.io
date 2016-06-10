1824d102deaa3d8cf7808af72582f274eeffbc5c
Cooper Johnston <sverige.cooper@gmail.com>
2016-06-10 13:27:33 -0700

    added User-Agent

--- api_client_script.js
+++ api_client_script.js
@@ -5,6 +5,10 @@ var newNation;
 var mode;
 var targets;
 var tgLimit;
+var request = new XMLHttpRequest();
+
+request.open("GET", "http://www.nationstates.net/cgi-bin/api.cgi", true);
+request.setRequestHeader("User-Agent", "http://thesolborg.github.io");
 
 function newRecruits() {
     mode = "new";
